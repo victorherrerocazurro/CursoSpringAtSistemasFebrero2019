43b5375a5a16b3e3e7137994e62913e1d9870235
Victor Herrero Cazurro <victorherrerocazurro@gmail.com>
2019-02-15 09:30:48 +0100

    OAuth2, Zipkin y Sleuth, Admin, Zuul y Hystrix

---
+++ workspace/27_Spring_Boot_Admin_with_Eureka/src/main/java/com/ejemplo/spring/admin/Application.java
@@ -0,0 +1,19 @@
+package com.ejemplo.spring.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaServer
+@EnableAdminServer
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
